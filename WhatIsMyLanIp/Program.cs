63d85199dd86c00e910f6cfd037f3d7c0d2f9d9e
David Valovage <david.valovage@gmail.com>
2020-10-24 13:52:51 -0600

    Added console app for detecting LAN IP address.

---
+++ WhatIsMyLanIp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WhatIsMyLanIp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                var hostName = Dns.GetHostName();
+                Console.WriteLine($"Computer Name: {hostName}");
+                IPHostEntry ipEntry = Dns.GetHostEntry(hostName);
+                var addresses = ipEntry.AddressList.Where(al => al.AddressFamily == AddressFamily.InterNetwork);
+
+                Console.WriteLine($"Type this IP address into the browser address bar: {addresses.FirstOrDefault()}");
+                Console.Read();
+                Console.Read();
+            }
+        }
+    }
+}
